6b561b631ce55711717be032760dbe06a9703523
jkbrdk <j.prudek@email.cz>
2024-10-04 20:06:12 +0200

    nahrani na github

---
+++ php/contactform.php
@@ -0,0 +1,40 @@
+<?php
+ // configuration
+ 
+$email_it_to = "info@raxidesign.cz";
+
+$error_message = "Prosím vyplňte všechna pole";
+
+$rnd=$_POST['rnd'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$body=$_POST['body'];
+
+  
+if(!isset($rnd) || !isset($name) || !isset($email) || !isset($subject) || !isset($body)) {
+	echo $error_message;
+    die();
+}
+
+
+$subject = stripslashes($subject);
+$email_from = $email;
+
+$email_message = "Message submitted by '".stripslashes($name)."', email:".$email_from;
+$email_message .=" on ".date("d/m/Y")."\n\n";
+$email_message .= stripslashes($body);
+$email_message .="\n\n";
+
+// Always set content-type when sending HTML email
+
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8". "\r\n";
+$headers .= 'From: '.stripslashes($name);
+
+mail($email_it_to,$subject,$email_message,$headers);
+
+
+
+?>
